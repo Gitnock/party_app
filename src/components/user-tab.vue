1d4da642beda7913e02d2119b950bb0a39538eed
Enock Lubega <enocklubega@gmail.com>
2021-08-26 05:17:57 -0400

    0.6.7

--- src/components/user-tab.vue
+++ src/components/user-tab.vue
@@ -62,7 +62,7 @@
             <div class="prof-bot"></div>
 
             <footer style="color: #626891" class="clickable">
-              <p>Privacy policy | v-0.6.6</p>
+              <p>Privacy policy | v-0.6.7</p>
             </footer>
           </div>
         </template>
